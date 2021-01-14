9e22269178d253ef876b0c387fbbd9f893db75a7
bardin08 <asdasfb326@gmail.com>
2021-01-14 12:58:05 +0200

    Update a prize model. Added constructors and id property. Corrected some spelling mistakes in the XML-documentation

--- TournamentTracker/TournamentTracker/Models/PrizeModel.cs
+++ TournamentTracker/TournamentTracker/Models/PrizeModel.cs
@@ -3,6 +3,10 @@
     public class PrizeModel
     {
         /// <summary>
+        /// Represents prize unique identifier.
+        /// </summary>
+        public int Id { get; set; }
+        /// <summary>
         /// Represents the place for which this prize will be given.
         /// </summary>
         public int PlaceNumber { get; set; }
@@ -11,16 +15,16 @@
         /// </summary>
         public string PlaceName { get; set; }
         /// <summary>
-        /// Represents a price if it`s a thing.
+        /// Represents a prize if it`s a thing.
         /// </summary
-        public string PriceName { get; set; }
+        public string PrizeName { get; set; }
         /// <summary>
         /// Represents the price amount.
         /// </summary>
         /// <remarks>
         /// Should be used only if a prize is a fixed amount of something.
         /// </remarks>
-        public decimal PriceAmount { get; set; }
+        public decimal PrizeAmount { get; }
         /// <summary>
         /// Represents which percent from a full prize fund will receive a person or a team.
         /// </summary>
@@ -28,5 +32,72 @@
         /// Should be used only if <see cref="PriceAmount"/> is not used.
         /// </remarks>
         public double PricePercentage { get; set; }
+
+        /// <summary>
+        /// Creates a prize model.
+        /// </summary>
+        public PrizeModel()
+        {
+        }
+        /// <summary>
+        /// Creates a prize model.
+        /// </summary>
+        /// <param name="placeNumber">Represents the place for which this prize will be given.</param>
+        /// <param name="placeName">Represents the place description.</param>
+        /// <param name="prizeName">Represents a prize if it`s a thing.</param>
+        /// <param name="prizeAmount">Represents which percent from a full prize fund will receive a person or a team.</param>
+        public PrizeModel(int placeNumber, string placeName, string prizeName, decimal prizeAmount)
+        {
+            if (string.IsNullOrEmpty(placeName))
+            {
+                throw new System.ArgumentException($"\"{nameof(placeName)}\" can`t be null or empty", nameof(placeName));
+            }
+
+            if (string.IsNullOrEmpty(prizeName))
+            {
+                throw new System.ArgumentException($"\"{nameof(prizeName)}\" can`t be null or empty", nameof(prizeName));
+            }
+
+            if (placeNumber < 1)
+            {
+                throw new System.ArgumentException($"\"{nameof(placeNumber)}\" can`t be less then 1", nameof(placeNumber));
+            }
+
+            PlaceNumber = placeNumber;
+            PlaceName = placeName;
+            PrizeName = prizeName;
+            PrizeAmount = prizeAmount;
+            PricePercentage = 0;
+        }
+        /// <summary>
+        /// Creates a prize model.
+        /// </summary>
+        /// <param name="placeNumber">Represents the place for which this prize will be given.</param>
+        /// <param name="placeName">Represents the place description.</param>
+        /// <param name="prizeName">Represents a prize if it`s a thing.</param>
+        /// <param name="pricePercentage">Represents which percent from a full prize fund will receive a person or a team.</param>
+        public PrizeModel(int placeNumber, string placeName, string prizeName, double pricePercentage)
+        {
+            if (string.IsNullOrEmpty(placeName))
+            {
+                throw new System.ArgumentException($"\"{nameof(placeName)}\" can`t be null or empty", nameof(placeName));
+            }
+
+            if (string.IsNullOrEmpty(prizeName))
+            {
+                throw new System.ArgumentException($"\"{nameof(prizeName)}\" can`t be null or empty", nameof(prizeName));
+            }
+
+            if (placeNumber < 1)
+            {
+                throw new System.ArgumentException($"\"{nameof(placeNumber)}\" can`t be less then 1", nameof(placeNumber));
+            }
+
+            PlaceNumber = placeNumber;
+            PlaceName = placeName;
+            PrizeName = prizeName;
+            PricePercentage = pricePercentage;
+            PrizeAmount = 0;
+        }
     }
 }
