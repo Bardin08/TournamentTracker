1d21a65e085b9bc8160ad3c3d8da2bec8f7b00d2
bardin08 <asdasfb326@gmail.com>
2021-01-14 16:45:16 +0200

    Rename MSSQLConnection to MSSQLConnector and move to other namespace

---
+++ TournamentTracker/TournamentTracker/Connectors/MSSQLConnector.cs
@@ -0,0 +1,33 @@
+﻿using Dapper;
+using System.Data;
+using System.Data.SqlClient;
+using TournamentTracker.Interfaces;
+using TournamentTracker.Models;
+
+namespace TournamentTracker.Connectors
+{
+    public class MSSQLConnector : IDataConnection
+    {
+        public PrizeModel CreatePrize(PrizeModel prize)
+        {
+            using (var connection = 
+                new SqlConnection(GlobalConfiguration.GetConnectionString("Tournaments")))
+            {
+                var p = new Dapper.DynamicParameters();
+
+                p.Add("@PlaceNumber", prize.PlaceNumber);
+                p.Add("@PlaceName", prize.PlaceName);
+                p.Add("@PrizeName", prize.PrizeName);
+                p.Add("@PrizeAmount", prize.PrizeAmount);
+                p.Add("@PrizePercentage", prize.PricePercentage);
+                p.Add("@id", prize.Id, DbType.Int32, ParameterDirection.Output);
+
+                connection.Execute("dbo.spPrizes_Insert", p, commandType: CommandType.StoredProcedure);
+
+                prize.Id = p.Get<int>("@id");
+
+                return prize;
+            }
+        }
+    }
+}
