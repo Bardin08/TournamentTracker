a6f5b15702f85b95df3789d95e5c8469b4271dcf
bardin08 <asdasfb326@gmail.com>
2021-01-15 14:39:09 +0200

    Add csv file connector

---
+++ TournamentTracker/TournamentTracker/Connectors/CSVConnector.cs
@@ -0,0 +1,31 @@
+﻿using System.Linq;
+using System.Collections.Generic;
+using TournamentTracker.Models;
+using TournamentTracker.Interfaces;
+using TournamentTracker.Connectors.TextHelpers;
+
+namespace TournamentTracker.Connectors
+{
+    public class CSVConnector : IDataConnection
+    {
+        public const string PrizesFilePath = "PrizeModels.csv";
+
+        public PrizeModel CreatePrize(PrizeModel prize)
+        {
+            List<PrizeModel> prizes = PrizesFilePath.GetFilePath().LoadFile().ConvertLinesToPrizeModels();
+            var lastPrizeId = 0;
+            
+            if (prizes.Count > 0)
+            {
+                lastPrizeId = prizes.OrderByDescending(x => x.Id).First().Id;
+            }
+
+            prize.Id = lastPrizeId + 1;
+
+            prizes.Add(prize);
+            prizes.SaveToPrizeFile(PrizesFilePath);
+
+            return prize;
+        }
+    }
+}
---
+++ TournamentTracker/TournamentTracker/Connectors/TextHelpers/CSVFileProcessor.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+using System.Configuration;
+using TournamentTracker.Models;
+using System.Collections.Generic;
+
+namespace TournamentTracker.Connectors.TextHelpers
+{
+    public static class CSVFileProcessor
+    {
+        public static string GetFilePath(this string fileName) 
+        {
+            var directory = Environment.GetFolderPath(Environment.SpecialFolder.ApplicationData) +
+                ConfigurationManager.AppSettings["DataFilesDirectory"];
+            
+            if (!Directory.Exists(directory))
+            {
+                Directory.CreateDirectory(directory);
+            }
+
+            return directory + $"\\{fileName}";
+        }
+
+        public static List<string> LoadFile(this string filePath)
+        {
+            if (!File.Exists(filePath))
+            {
+                return new List<string>();
+            }
+
+            return File.ReadAllLines(filePath).ToList();
+        }
+
+        public static List<PrizeModel> ConvertLinesToPrizeModels(this List<string> lines)
+        {
+            var output = new List<PrizeModel>();
+            
+            foreach (var line in lines)
+            {
+                var cols = line.Split(',');
+
+                var m = new PrizeModel()
+                {
+                    Id = int.Parse(cols[0]),
+                    PlaceNumber = int.Parse(cols[1]),
+                    PlaceName = cols[2],
+                    PrizeName = cols[3],
+                    PrizeAmount = decimal.Parse(cols[4]),
+                    PrizePercentage = double.Parse(cols[5])
+                };
+
+                output.Add(m);
+            }
+
+            return output;
+        }
+
+        public static void SaveToPrizeFile(this List<PrizeModel> prizes, string fileName)
+        {
+            var lines = new List<string>();
+
+            foreach (var prize in prizes)
+            {
+                lines.Add($"{prize.Id},{prize.PlaceNumber},{prize.PlaceName},{prize.PrizeName},{prize.PrizeAmount},{prize.PrizePercentage}");
+            }
+
+            File.WriteAllLinesAsync(fileName.GetFilePath(), lines);
+        }
+    }
+}
