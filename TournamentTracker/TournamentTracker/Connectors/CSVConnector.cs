1d8216d8cfa10d5cf1efd0b23e342eb9aec3f208
bardin08 <asdasfb326@gmail.com>
2021-01-16 19:54:09 +0200

    Add create person and team method to CSV connector

--- TournamentTracker/TournamentTracker/Connectors/CSVConnector.cs
+++ TournamentTracker/TournamentTracker/Connectors/CSVConnector.cs
@@ -11,12 +11,13 @@ namespace TournamentTracker.Connectors
     /// </summary>
     public class CSVConnector : IDataConnection
     {
-        public const string PrizesFilePath = "PrizeModels.csv";
-        public const string PeopleFilePath = "PeopleModels.csv";
+        private const string PrizesFileName = "PrizeModels.csv";
+        private const string PeopleFileName = "PeopleModels.csv";
+        private const string TeamsFileName = "TeamModels.csv";
 
         public PersonModel CreatePerson(PersonModel person)
         {
-            List<PersonModel> people = PeopleFilePath.GetFilePath().LoadFile().ConvertLinesToPersonModels();
+            List<PersonModel> people = PeopleFileName.GetFilePath().LoadFile().ConvertLinesToPersonModels();
 
             var lastId = 0;
 
@@ -28,14 +29,14 @@ namespace TournamentTracker.Connectors
             person.Id = lastId + 1;
 
             people.Add(person);
-            people.SaveToPeopleFile(PeopleFilePath);
+            people.SaveToPeopleFile(PeopleFileName);
 
             return person;
         }
 
         public PrizeModel CreatePrize(PrizeModel prize)
         {
-            List<PrizeModel> prizes = PrizesFilePath.GetFilePath().LoadFile().ConvertLinesToPrizeModels();
+            List<PrizeModel> prizes = PrizesFileName.GetFilePath().LoadFile().ConvertLinesToPrizeModels();
             var lastPrizeId = 0;
             
             if (prizes.Count > 0)
@@ -46,9 +47,33 @@ namespace TournamentTracker.Connectors
             prize.Id = lastPrizeId + 1;
 
             prizes.Add(prize);
-            prizes.SaveToPrizesFile(PrizesFilePath);
+            prizes.SaveToPrizesFile(PrizesFileName);
 
             return prize;
+        }
+
+        public TeamModel CreateTeam(TeamModel team)
+        {
+            var teams = TeamsFileName.GetFilePath().LoadFile().ConvertLinesToTeamModels(PeopleFileName);
+
+            int lastId = 0;
+
+            if (teams.Count > 0)
+            {
+                lastId = teams.OrderByDescending(x => x.Id).First().Id;
+            }
+
+            team.Id = lastId + 1;
+
+            teams.Add(team);
+            teams.SaveToTeamsFile(TeamsFileName);
+
+            return team;
+        }
+
+        public List<PersonModel> GetAllParticipants()
+        {
+            return PeopleFileName.GetFilePath().LoadFile().ConvertLinesToPersonModels();
         }
     }
 }
