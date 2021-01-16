e2bc0f379d2968fdf1600d44204ff37cd5dc2e89
bardin08 <asdasfb326@gmail.com>
2021-01-16 19:52:32 +0200

    Update team model

--- TournamentTracker/TournamentTracker/Models/TeamModel.cs
+++ TournamentTracker/TournamentTracker/Models/TeamModel.cs
@@ -5,6 +5,10 @@ namespace TournamentTracker.Models
     public class TeamModel
     {
         /// <summary>
+        /// Represents team unique identifier.
+        /// </summary>
+        public int Id { get; set; }
+        /// <summary>
         /// Represents the team name.
         /// </summary>
         public string TeamName { get; set; }
@@ -12,5 +16,23 @@ namespace TournamentTracker.Models
         /// Represents the team members.
         /// </summary>
         public List<PersonModel> TeamMembers { get; set; } = new List<PersonModel>();
+
+        /// <summary>
+        /// Constructs a team model.
+        /// </summary>
+        public TeamModel()
+        {
+        }
+
+        /// <summary>
+        /// Constructs a team model.
+        /// </summary>
+        /// <param name="teamName"> Team name. </param>
+        /// <param name="teamMembers"> A list of person models which represents team members. </param>
+        public TeamModel(string teamName, List<PersonModel> teamMembers)
+        {
+            TeamName = teamName ?? throw new System.ArgumentNullException(nameof(teamMembers));
+            TeamMembers = teamMembers ?? throw new System.ArgumentNullException(nameof(teamName));
+        }
     }
 }
