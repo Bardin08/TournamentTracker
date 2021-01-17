dd110b6472453ebabe40d2e0dc62325034d10467
bardin08 <asdasfb326@gmail.com>
2021-01-17 23:37:30 +0200

    Create two interfaces to allow team creation form and prize creation from to return recently created object

---
+++ TournamentTracker/TournamentTrackerWPFUI/Interfaces/IPrizeRequester.cs
@@ -0,0 +1,9 @@
+﻿using TournamentTracker.Models;
+
+namespace TournamentTrackerWPFUI.Interfaces
+{
+    public interface IPrizeRequester
+    {
+        void PrizeCreated(PrizeModel prize);        
+    }
+}
