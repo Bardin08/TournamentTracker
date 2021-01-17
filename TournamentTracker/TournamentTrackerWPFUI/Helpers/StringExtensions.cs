f0638a12797a45e5f160bfeab696c0f1fc491551
bardin08 <asdasfb326@gmail.com>
2021-01-17 23:31:40 +0200

    Add several string extension methods

---
+++ TournamentTracker/TournamentTrackerWPFUI/Helpers/StringExtensions.cs
@@ -0,0 +1,39 @@
+﻿using System.Text;
+using System.Collections.Generic;
+
+namespace TournamentTrackerWPFUI.Helpers
+{
+    public static class StringExtensions
+    {
+        /// <summary>
+        /// Indicates whether the specified array of strings contains strings that are
+        /// null, ("") or consists only of whitespace characters.
+        /// </summary>
+        /// <param name="strings"> An array of strings to check. </param>
+        /// <returns></returns>
+        public static bool IsNullEmptyOrWhitespace(this string[] strings)
+        {
+            foreach (var str in strings)
+            {
+                if (string.IsNullOrEmpty(str) || string.IsNullOrWhiteSpace(str))
+                {
+                    return true;
+                }
+            }
+
+            return false;
+        }
+
+        public static string GetValidationErrorMessage(this List<string> errors)
+        {
+            var sb = new StringBuilder("Errors:\n");
+
+            foreach (var error in errors)
+            {
+                sb.Append("  • ").Append(error).Append(";\n");
+            }
+
+            return sb.ToString();
+        }
+    }
+}
