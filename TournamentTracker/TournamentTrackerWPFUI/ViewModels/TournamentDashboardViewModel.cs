775926cbc469ed0968c7391c09cc6faf849c38e4
bardin08 <asdasfb326@gmail.com>
2021-01-20 23:52:44 +0200

    Add tournament dashboard view mode

---
+++ TournamentTracker/TournamentTrackerWPFUI/ViewModels/TournamentDashboardViewModel.cs
@@ -0,0 +1,34 @@
+﻿using TournamentTracker.Models;
+using System.Collections.ObjectModel;
+using System.Collections.Specialized;
+
+namespace TournamentTrackerWPFUI.ViewModels
+{
+    public class TournamentDashboardViewModel : INotifyCollectionChanged
+    {
+        private ObservableCollection<TournamentModel> _tournaments;
+
+        public event NotifyCollectionChangedEventHandler CollectionChanged;
+
+        public ObservableCollection<TournamentModel> Tournaments
+        {
+            get { return _tournaments; }
+            set 
+            {
+                _tournaments = value;
+                CollectionChanged?.Invoke(this, new NotifyCollectionChangedEventArgs(NotifyCollectionChangedAction.Reset));
+            }
+        }
+
+        public TournamentModel SelectedTournament { get; set; }
+
+        public TournamentDashboardViewModel()
+        {
+            // TODO: Realize tournament loading
+            Tournaments =
+                new ObservableCollection<TournamentModel>(TournamentTracker.GlobalConfiguration.Connection.GetTournaments());
+
+            SelectedTournament = Tournaments[0];
+        }
+    }
+}
