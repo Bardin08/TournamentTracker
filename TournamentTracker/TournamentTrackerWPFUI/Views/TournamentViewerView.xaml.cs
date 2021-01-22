476f73ea9eea74f68963734f8fe4766241efc6eb
Vladislav Bardin <asdasfb326@gmail.com>
2021-01-22 23:37:48 +0200

    Create new tournament viewer view

--- TournamentTracker/TournamentTrackerWPFUI/Views/TournamentViewerView.xaml.cs
+++ TournamentTracker/TournamentTrackerWPFUI/Views/TournamentViewerView.xaml.cs
@@ -1,41 +1,72 @@
 ﻿using MahApps.Metro.Controls;
+using MahApps.Metro.Controls.Dialogs;
+using System.Linq;
+using TournamentTracker;
 using TournamentTracker.Models;
 using TournamentTrackerWPFUI.ViewModels;
 
 namespace TournamentTrackerWPFUI.Views
 {
     /// <summary>
-    /// Interacting logic for TournamentViewerView.xaml
+    /// Interaction logic for TournamentViewerView.xaml
     /// </summary>
     public partial class TournamentViewerView : MetroWindow
     {
-        private readonly TournamentViewverViewModel _viewModel;
- 
+        private readonly TournamentViewerViewModel _viewModel;
+
         public TournamentViewerView(TournamentModel tournament)
         {
             InitializeComponent();
 
-            _viewModel = new TournamentViewverViewModel(tournament);
+            _viewModel = new TournamentViewerViewModel(GlobalConfiguration.Connection, tournament);
 
             DataContext = _viewModel;
         }
 
-        private void SubmitScoreButton_Click(object sender, System.Windows.RoutedEventArgs e)
+        private void RoundComboBox_SelectionChanged(object sender, System.Windows.Controls.SelectionChangedEventArgs e)
         {
+            _viewModel.CurrentRound = (int)RoundComboBox.SelectedItem;
         }
 
         private void MatchesListBox_SelectionChanged(object sender, System.Windows.Controls.SelectionChangedEventArgs e)
         {
-            _viewModel.SelectMatch(MatchesListBox.SelectedItem as MatchModel);
+            _viewModel.SelectedMatch = (MatchesListBox.SelectedItem as MatchModel);
         }
 
-        private void OnlyUnplayedMatchesCheckBox_Checked(object sender, System.Windows.RoutedEventArgs e)
+        private void ShowOnlyUnplayedCheckBox_Checked(object sender, System.Windows.RoutedEventArgs e)
         {
-            _viewModel.ShowOnlyUnplayed((bool)OnlyUnplayedMatchesCheckBox.IsChecked);
+            _viewModel.ShowOnlyUnplayed = true;
         }
 
-        private void RoundSplitButton_SelectionChanged(object sender, System.Windows.Controls.SelectionChangedEventArgs e)
+        private void ShowOnlyUnplayedCheckBox_Unchecked(object sender, System.Windows.RoutedEventArgs e)
         {
+            _viewModel.ShowOnlyUnplayed = false;
+        }
+
+        private async void SubmitScoreButton_Click(object sender, System.Windows.RoutedEventArgs e)
+        {
+            if (ValidateScore())
+            {
+                int firstTeamScore = int.Parse(FirstTeamScoreTextBox.Text);
+                int secondTeamScore = int.Parse(SecondTeamScoreTextBox.Text);
+
+                _viewModel.WriteMatchResult(firstTeamScore, secondTeamScore);
+            }
+            else
+            {
+                await this.ShowMessageAsync("", "Incorrect score");
+            }
+        }
+
+        private bool ValidateScore()
+        {
+            var teamsScoreCorrect = int.TryParse(FirstTeamScoreTextBox.Text, out int firstTeamScore) &&
+                int.TryParse(SecondTeamScoreTextBox.Text, out int secondTeamScore) &&
+                firstTeamScore >= 0 && 
+                secondTeamScore >= 0 &&
+                firstTeamScore != secondTeamScore;
+
+            return teamsScoreCorrect;
         }
     }
 }
