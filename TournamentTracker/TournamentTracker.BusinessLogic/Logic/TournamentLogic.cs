d98350c9f2270a23e74f6efbec9fd23b23fdd861
Vladislav Bardin <asdasfb326@gmail.com>
2021-01-29 21:35:21 +0200

    Move rounds creation logic to tournament model

--- TournamentTracker/TournamentTracker.BusinessLogic/Logic/TournamentLogic.cs
+++ TournamentTracker/TournamentTracker.BusinessLogic/Logic/TournamentLogic.cs
@@ -1,7 +1,5 @@
-﻿using System;
-using System.Linq;
+﻿using System.Linq;
 using System.Text;
-using System.Collections.Generic;
 
 using TournamentTracker.Domain.Models;
 
@@ -9,101 +7,7 @@ namespace TournamentTracker.BusinessLogic.Logic
 {
     public static class TournamentLogic
     {
-
-        // REFACTOR: Move to a tournament model. GRASP patterns
-        public static void CreateRounds(TournamentModel tournament)
-        {
-            List<TeamModel> shuffledTeams = ShuffleTeams(tournament.EnteredTeams);
-            int rounds = FindNumberOfRounds(shuffledTeams.Count);
-            int byes = FindNumberOfByes(shuffledTeams.Count, rounds);
-
-            tournament.Rounds.Add(CreateFirstRound(byes, shuffledTeams));
-            CreateOtherRounds(tournament, rounds);
-        }
-
-        private static void CreateOtherRounds(TournamentModel tournament, int rounds)
-        {
-            int round = 2;
-            List<MatchModel> previousRound = tournament.Rounds[0];
-            List<MatchModel> currentRound = new List<MatchModel>();
-            MatchModel currentMatch = new MatchModel();
-
-            while (round <= rounds)
-            {
-                foreach (var match in previousRound)
-                {
-                    currentMatch.Entries.Add(new MatchEntryModel { ParentMatch = match });
-
-                    if (currentMatch.Entries.Count > 1)
-                    {
-                        currentMatch.RoundNumber = round;
-                        currentRound.Add(currentMatch);
-                        currentMatch = new MatchModel();
-                    }
-                }
-                tournament.Rounds.Add(currentRound);
-                previousRound = currentRound;
-
-                currentRound = new List<MatchModel>();
-                ++round;
-            }
-        }
-
-        private static List<MatchModel> CreateFirstRound(int byes, List<TeamModel> teams)
-        {
-            List<MatchModel> output = new List<MatchModel>();
-            MatchModel currentMatch = new MatchModel();
-
-            foreach (var team in teams)
-            {
-                currentMatch.Entries.Add(new MatchEntryModel { CompetingTeam = team });
-
-                if (byes > 0 || currentMatch.Entries.Count > 1)
-                {
-                    currentMatch.RoundNumber = 1;
-                    output.Add(currentMatch);
-                    currentMatch = new MatchModel();
-
-                    if (byes > 0)
-                    {
-                        --byes; 
-                    }
-                }
-            }
-
-            return output;
-        }
-
-        private static int FindNumberOfByes(int teamsAmount, int rounds)
-        {
-            int totalTeams = 1;
-
-            for (int i = 1; i <= rounds; i++)
-            {
-                totalTeams *= 2;
-            }
-
-            return totalTeams - teamsAmount;
-        }
-
-        private static int FindNumberOfRounds(int teamsAmount)
-        {
-            int output = 1, val = 2;
-            
-            while (val < teamsAmount)
-            {
-                ++output;
-                val *= 2;
-            }
-
-            return output;
-        }
-
-        private static List<TeamModel> ShuffleTeams(List<TeamModel> teams)
-        {
-            return teams.OrderBy(x => Guid.NewGuid()).ToList();
-        }
-
+        // REFACTOR: Move to notifier
         public static void Notify(TournamentModel tournament, MatchModel match)
         {
             var sb = new StringBuilder();
--- TournamentTracker/TournamentTracker.Domain/Models/TournamentModel.cs
+++ TournamentTracker/TournamentTracker.Domain/Models/TournamentModel.cs
@@ -1,4 +1,6 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
 
 namespace TournamentTracker.Domain.Models
 {
@@ -33,5 +35,122 @@ namespace TournamentTracker.Domain.Models
         /// Represents the tournament rounds.
         /// </summary>
         public List<List<MatchModel>> Rounds { get; set; } = new List<List<MatchModel>>();
+    
+
+        /// <summary>
+        /// Create rounds for the current tournament model.
+        /// </summary>
+        public void CreateRounds()
+        {
+            EnteredTeams = ShuffleTeams(EnteredTeams);
+            int rounds = FindNumberOfRounds();
+            int byes = FindNumberOfByes();
+
+            Rounds.Add(CreateFirstRound(byes));
+            CreateOtherRounds(rounds);
+        }
+
+        /// <summary>
+        /// Create matches for the first round.
+        /// </summary>
+        /// <param name="byes"> Number of byes. </param>
+        /// <returns></returns>
+        private List<MatchModel> CreateFirstRound(int byes)
+        {
+            List<MatchModel> output = new List<MatchModel>();
+            MatchModel currentMatch = new MatchModel();
+
+            foreach (var team in EnteredTeams)
+            {
+                currentMatch.Entries.Add(new MatchEntryModel { CompetingTeam = team });
+
+                if (byes > 0 || currentMatch.Entries.Count > 1)
+                {
+                    currentMatch.RoundNumber = 1;
+                    output.Add(currentMatch);
+                    currentMatch = new MatchModel();
+
+                    if (byes > 0)
+                    {
+                        --byes;
+                    }
+                }
+            }
+
+            return output;
+        }
+
+        /// <summary>
+        /// Create matches for all rounds except first.
+        /// </summary>
+        /// <param name="rounds"> Rounds amount. </param>
+        private void CreateOtherRounds(int rounds)
+        {
+            int round = 2;
+            List<MatchModel> previousRound = Rounds[0];
+            List<MatchModel> currentRound = new List<MatchModel>();
+            MatchModel currentMatch = new MatchModel();
+
+            while (round <= rounds)
+            {
+                foreach (var match in previousRound)
+                {
+                    currentMatch.Entries.Add(new MatchEntryModel { ParentMatch = match });
+
+                    if (currentMatch.Entries.Count > 1)
+                    {
+                        currentMatch.RoundNumber = round;
+                        currentRound.Add(currentMatch);
+                        currentMatch = new MatchModel();
+                    }
+                }
+                Rounds.Add(currentRound);
+                previousRound = currentRound;
+
+                currentRound = new List<MatchModel>();
+                ++round;
+            }
+        }
+
+        /// <summary>
+        /// Returns the number of byes that should be added.
+        /// </summary>
+        /// <returns></returns>
+        private int FindNumberOfByes()
+        {
+            int totalTeams = 1;
+
+            for (int i = 1; i <= FindNumberOfRounds(); i++)
+            {
+                totalTeams *= 2;
+            }
+
+            return totalTeams - EnteredTeams.Count;
+        }
+
+        /// <summary>
+        /// Return the number of matches which will be played.
+        /// </summary>
+        private int FindNumberOfRounds()
+        {
+            int output = 1, val = 2;
+
+            while (val < EnteredTeams.Count)
+            {
+                ++output;
+                val *= 2;
+            }
+
+            return output;
+        }
+
+        /// <summary>
+        /// Shuffle teams and return a shuffled list.
+        /// </summary>
+        /// <param name="teams"> The list of teams. </param>
+        private List<TeamModel> ShuffleTeams(List<TeamModel> teams)
+        {
+            return teams.OrderBy(x => Guid.NewGuid()).ToList();
+        }
     }
 }
